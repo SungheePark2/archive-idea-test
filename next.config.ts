c005694badb5d769b33cc1303c0af96f5f63b9ce
Sunghee Park <tkrhktyd@naver.com>
2025-07-06 18:38:51 +0900

    first init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+  images: {
+    domains: ["localhost"],
+  },
+
 };
 
 export default nextConfig;
